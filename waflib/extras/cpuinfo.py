e8dc45d62cad942165e931c65df71c7060897a33
Kean H Lau <khklau@users.noreply.github.com>
2015-01-14 18:44:49 +1100

    adding in find_pointer_size

--- waflib/extras/cpuinfo.py
+++ waflib/extras/cpuinfo.py
@@ -13,8 +13,8 @@ When using this tool, the wscript will look like:
 
     def configure(conf):
 	conf.load('compiler_cxx cpuinfo')
-	dcacheline = conf.find_dcache_line_size()
-	conf.env.append_value('CXXFLAGS', ['-DDCACHE_LINESIZE=%s' % dcacheline])
+	conf.env.append_value('CXXFLAGS', ['-DDCACHE_LINESIZE=%s' % conf.find_dcache_line_size()])
+	conf.env.append_value('CXXFLAGS', ['-m%s' % conf.find_pointer_size()])
 '''
 
 import sys
@@ -30,6 +30,18 @@ def find_dcache_line_size(self):
 	result = Popen('getconf LEVEL1_DCACHE_LINESIZE', shell=True, stdout=PIPE).stdout.readline().strip()
     if result != '':
 	self.end_msg('%s bytes' % result)
+    else:
+	self.fatal('could not be detected')
+    return result
+
+@conf
+def find_pointer_size(self):
+    self.start_msg('Detecting native pointer size')
+    result = ''
+    if sys.platform == 'linux2':
+	result = Popen('getconf LONG_BIT', shell=True, stdout=PIPE).stdout.readline().strip()
+    if result != '':
+	self.end_msg('%s bits' % result)
     else:
 	self.fatal('could not be detected')
     return result
