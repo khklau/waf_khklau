79864983f589054e4c37dbcef72f809dd0c8daf5
Kean H Lau <khklau@users.noreply.github.com>
2015-02-08 21:07:08 +1100

    updating references from all to preparation

--- waflib/extras/preparation.py
+++ waflib/extras/preparation.py
@@ -4,21 +4,21 @@
 
 '''
 This is an extra tool, not bundled with the default waf binary.
-To add the All Command tool to the waf file:
-    $ ./waf-light --tools=all_cmd
+To add the Preparation tool to the waf file:
+    $ ./waf-light --tools=preparation
 or, if you have waf >= 1.6.2
-    $ ./waf update --files=all_cmd
+    $ ./waf update --files=preparation
 
 When using this tool, the wscript will look like:
 
-    from waflib.extras.all_cmd import AllContext
+    from waflib.extras.preparation import PreparationContext
 
-    def all(allCtx):
-	allCtx.msg('my option = ', allCtx.options.my)
+    def prepare(prepCtx):
+	prepCtx.msg('my option = ', prepCtx.options.my)
 '''
 
 from waflib.Configure import ConfigurationContext
 
-class AllContext(ConfigurationContext):
-    cmd = 'all'
-    fun = 'all'
+class PreparationContext(ConfigurationContext):
+    cmd = 'prepare'
+    fun = 'prepare'
