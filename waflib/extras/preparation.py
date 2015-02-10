60a83af74983bed66d812ab415a2aad4dff7b716
Kean H Lau <khklau@users.noreply.github.com>
2015-02-11 09:54:46 +1100

    PreparationContext needs to implement it's own load method because the method from ConfigurationContext is not reusable

--- waflib/extras/preparation.py
+++ waflib/extras/preparation.py
@@ -18,7 +18,63 @@ When using this tool, the wscript will look like:
 '''
 
 from waflib.Configure import ConfigurationContext
+from waflib import Utils, Options, Context
 
 class PreparationContext(ConfigurationContext):
+
     cmd = 'prepare'
     fun = 'prepare'
+
+    def __init__(self, **kw):
+	super(PreparationContext, self).__init__(**kw)
+
+    def load(self, input, tooldir=None, funs=None, download=False):
+	"""
+	Load Waf tools, which will be imported whenever a build is started.
+
+	:param input: waf tools to import
+	:type input: list of string
+	:param tooldir: paths for the imports
+	:type tooldir: list of string
+	:param funs: functions to execute from the waf tools
+	:type funs: list of string
+	:param download: whether to download the tool from the waf repository
+	:type download: bool
+	"""
+	# ConfigurationContext.load isn't reusable, so it ends up being nostly duplicated here
+	tools = Utils.to_list(input)
+	if tooldir: tooldir = Utils.to_list(tooldir)
+	for tool in tools:
+	    # avoid loading the same tool more than once with the same functions
+	    # used by composite projects
+
+	    mag = (tool, id(self.env), funs)
+	    if mag in self.tool_cache:
+		self.to_log('(tool %s is already loaded, skipping)' % tool)
+		continue
+	    self.tool_cache.append(mag)
+
+	    module = None
+	    try:
+		module = Context.load_tool(tool, tooldir)
+	    except ImportError as e:
+		if Options.options.download:
+		    module = download_tool(tool, ctx=self)
+		    if not module:
+			self.fatal('Could not load the Waf tool %r or download a suitable replacement from the repository (sys.path %r)\n%s' % (tool, sys.path, e))
+		else:
+		    self.fatal('Could not load the Waf tool %r from %r (try the --download option?):\n%s' % (tool, sys.path, e))
+	    except Exception as e:
+		self.to_log('imp %r (%r & %r)' % (tool, tooldir, funs))
+		self.to_log(Utils.ex_stack())
+		raise
+
+	    if funs is not None:
+		self.eval_rules(funs)
+	    else:
+		func = getattr(module, 'prepare', None)
+		if func:
+		    if type(func) is type(Utils.readf): func(self)
+		    else: self.eval_rules(func)
+
+		self.tools.append({'tool':tool, 'tooldir':tooldir, 'funs':funs})
