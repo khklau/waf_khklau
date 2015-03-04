e51104fe6b71b7a3ecb880e7a4bc110e01b0fd2f
Kean H Lau <khklau@users.noreply.github.com>
2015-03-04 23:18:15 +1100

    implemented the prepare command

--- waflib/extras/protobuf.py
+++ waflib/extras/protobuf.py
@@ -26,15 +26,18 @@ Options available are:
     --protobuf-libpath : the directory containing the library files
 '''
 
-import sys
-import re
 import os
-from os.path import join
-import waflib.Build
-from waflib import Utils
+import re
+import sys
+import subprocess
+import tempfile
+import urllib
+import zipfile
+from waflib import Utils, Context
 from waflib.Configure import conf
-from waflib.Task import Task
-from waflib.TaskGen import extension 
+
+BOOTSTRAP_URL = 'https://github.com/khklau/protobuf_bootstrap/archive/%s'
+BOOTSTRAP_FILE = 'protobuf_bootstrap-%s.zip'
 
 def define_task_gen(context, **keywords):
     context(name=keywords['name'],
@@ -59,8 +62,74 @@ def options(optCtx):
 	    help='''absolute path to the protobuf libraries
 	    e.g. /path/to/protobuf/lib''')
 
-def prepare(prep):
-	pass
+def prepare(prepCtx):
+    if not prepCtx.env.dep_directory.contains('protobuf'):
+	return
+    productPath = ''
+    product = prepCtx.env.dep_directory.find('protobuf')
+    if len(product.dep_file_path) > 0:
+	productPath = os.path.dirname(product.dep_file_path)
+    else:
+	productPath = os.path.join(prepCtx.options.dep_base_dir,
+		'%s-%s' % (product.getName(), product.getVersion()))
+    topWscript = os.path.join(productPath, Context.WSCRIPT_FILE)
+    if not os.path.exists(topWscript) or not os.access(topWscript, os.R_OK):
+	if os.path.isdir(productPath):
+	    shutil.rmtree(productPath)
+	else:
+	    os.remove(productPath)
+	os.mkdir(productPath)
+	tempDir = tempfile.mkdtemp(prefix='protobuf_prepare-')
+	try:
+	    if product.getVersion().lower() == 'master':
+		sourceFile = 'master.zip'
+	    else:
+		sourceFile = BOOTSTRAP_FILE % product.getVersion()
+	    url = BOOTSTRAP_URL % sourceFile
+	    filePath = os.path.join(tempDir, sourceFile)
+	    prepCtx.start_msg('Downloading %s' % url)
+	    triesRemaining = 10
+	    while triesRemaining > 1:
+		try:
+		    urllib.urlretrieve(url, filePath)
+		    break
+		except urllib.ContentTooShortError:
+		    triesRemaining -= 1
+		if os.path.exists(filePath):
+		    os.remove(filePath)
+		else:
+		    prepCtx.fatal('Could not download %s' % url)
+	    handle = zipfile.Zipfile(filePath, 'r')
+	    handle.extractall(productPath)
+	finally:
+	    shutil.rmtree(tempDir)
+    wafExe = os.path.abspath(sys.argv[0])
+    cwd = os.getcwd()
+    try:
+	os.chdir(productPath)
+	prepCtx.msg('Preparing Protocol Buffers dependency in', productPath)
+	returnCode = subprocess.call([
+		wafExe,
+		'prepare',
+		'configure',
+		'build'])
+	if returnCode != 0:
+	    prepCtx.fatal('Protocol Buffers preparation failed: %d' % returnCode)
+	else:
+	    binPath = os.path.join(productPath, 'bin')
+	    inclPath = os.path.join(productPath, 'include')
+	    libPath = os.path.join(productPath, 'lib')
+	    if os.path.isdir(binPath) and os.path.isdir(inclPath) and os.path.isdir(libPath):
+		prepCtx.msg('Setting Protocol Buffers option protobuf_binpath to', binPath)
+		prepCtx.options.protobuf_binpath = binPath
+		prepCtx.msg('Setting Protocol Buffers option protobuf_incpath to', inclPath)
+		prepCtx.options.protobuf_incpath = inclPath
+		prepCtx.msg('Setting Protocol Buffers option protobuf_libpath to', libPath)
+		prepCtx.options.protobuf_libpath = libPath
+	    else:
+		prepCtx.fatal('Protocol Buffers preparation failed: %s and %s not found' % (inclPath, libPath))
+    finally:
+	os.chdir(cwd)
 
 @conf
 def check_protobuf(self):
@@ -69,13 +138,13 @@ def check_protobuf(self):
 	self.fatal('%s is not executable' % self.env['PROTOC'])
     self.end_msg('ok')
     self.start_msg('Checking Protocol Buffer headers')
-    headerPath = join(self.env['INCLUDES_PROTOBUF'], 'google', 'protobuf', 'message.h')
+    headerPath = os.path.join(self.env['INCLUDES_PROTOBUF'], 'google', 'protobuf', 'message.h')
     if not os.access(headerPath, os.R_OK):
 	self.fatal('%s is not readable' % headerPath)
     self.end_msg('ok')
     self.start_msg('Checking Protocol Buffer libraries')
     for lib in self.env['LIB_PROTOBUF']: 
-	libPath = join(self.env['LIBPATH_PROTOBUF'], "lib%s.so" % lib)
+	libPath = os.path.join(self.env['LIBPATH_PROTOBUF'], "lib%s.so" % lib)
 	if not os.access(libPath, os.R_OK):
 	    self.fatal('%s is not readable' % libPath)
     self.end_msg('ok')
@@ -93,9 +162,9 @@ def configure(confCtx):
 	binpath = '/usr/local/bin'
 
     if platform == 'win32':
-	protocpath = join(binpath, 'protoc.exe')
+	protocpath = os.path.join(binpath, 'protoc.exe')
     else:
-	protocpath = join(binpath, 'protoc')
+	protocpath = os.path.join(binpath, 'protoc')
 
     if confCtx.options.protobuf_incpath is not None and confCtx.options.protobuf_incpath != '':
 	incpath = confCtx.options.protobuf_incpath
